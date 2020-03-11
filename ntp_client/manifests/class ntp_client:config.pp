5ed2eb1fd9810a41d4a35683dfa9d7327b11dbe8
adelany <alex.delany@optima-cc.com>
2020-03-11 15:10:07 +0000

    First commit

---
+++ ntp_client/manifests/class ntp_client:config.pp
@@ -0,0 +1,34 @@
+class ntp_client::config (
+  $ntp_servers
+)
+{
+  package { 'ntp':
+    ensure => installed,
+  }
+
+  file { '/etc/ntp.conf':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template("${module_name}/ntp.conf.erb"),
+  }
+
+  file { '/etc/sysconfig/ntpd':
+    ensure => present,
+    path   => '/etc/sysconfig/ntpd',
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => ['puppet:///modules/ntp_client/ntpd'],
+    notify => Service['ntpd'],
+  }
+
+  service { 'ntpd':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/ntp.conf'],
+  }
+
+  Package['ntp'] -> File['/etc/ntp.conf'] -> Service['ntpd']
+}
--- ntp_client/manifests/init.pp
+++ ntp_client/manifests/init.pp
@@ -1,3 +1,11 @@
 class ntp_client () {
-  case $facts['os']['name'] {
+      case $facts['os']['name'] {
+    /RedHat|CentOS/: {
+      contain dns_resolv::config
+    }
+    default: {
+      fail('Unsupported OS')
+    }
+  }
+}
 
